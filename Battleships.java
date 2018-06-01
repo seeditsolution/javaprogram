a2703a8bfe51683aed52f0a55e50e888c5f308ef
Kimi450 <karan_samani@yahoo.co.in>
2018-06-01 14:00:21 +0530

    Added an indication of which ship is being placed. Fixed an oversight of ensuring the range was correct for the ship's placement (less than 0 was not catered for, now it is). Added checks to make sure you cant place a ship on another ship. Moved the scanner to be a general object so I need to close it only once, caused issues otherwise. Implemented a basic hit system with guesses taken in from user input. Removed a commented line from Main.Java.

--- Battleships.java
+++ Battleships.java
@@ -1,20 +1,36 @@
 
 import java.util.Scanner;
 
+/*
+ * Table
+ * 0 Empty
+ * 1 Ship present
+ * 2 bomb dopped on empty cell
+ * 3 bomb dropped on ship
+ */
 
 public class Battleships{
     private int model[][];
     private final int xSize;
     private final int ySize;
     private final static String SEPERATOR = "|";
+    private int ships;
+    private Scanner scanner;
+    private int bombsDropped=0;
+
 
     public Battleships(int x,int y, int ships){
         this.xSize = x;
         this.ySize = y;
+        this.ships = ships;
         model = new int[x][y];
+        scanner = new Scanner(System.in);
+
         inistialiseModel();
-        setShipRoutine(ships);
-        printModel();
+        setShipRoutine(this.ships);
+        guessRoutine();
+
+        scanner.close();
     }
 
     private void inistialiseModel(){
@@ -27,55 +43,89 @@ public class Battleships{
     }
 
     public void printModel(){
+        System.out.print("\n"+ SEPERATOR + " " + SEPERATOR);
+
+        for(int y=0; y<this.ySize; y++){
+            System.out.print(String.valueOf(y) + SEPERATOR);
+        }
+        System.out.println();
+
         for(int x=0; x<this.xSize; x++){
+            System.out.print(SEPERATOR + String.valueOf(x));
             for(int y=0; y<this.ySize; y++){
                 System.out.print(SEPERATOR);
                 int value = model[x][y];
                 if(value==0 || value==1){//emtpy or ship
                     if (value==1){
                         System.out.print("S");
-
                     }else {
                         System.out.print(" ");
                     }
-                // }else if (value==1) {
-                //     System.out.print(model[x][y]);
-                // }else if (value==-1) {
-                //     System.out.print(model[x][y]);
-                }else if (value==2) {//hit
+                }else if (value==2) {//misplaced bomb
+                    System.out.print("-");
+                }else if (value==3) {//hit
                     System.out.print("H");
                 }
             }
             System.out.println(SEPERATOR);
         }
+        System.out.println();
     }
 
     public void setShipRoutine(int times) {
-        Scanner scan = new Scanner(System.in);
         int x=xSize;
         int y=ySize;
         for (int i=0;i<times;i++){
-            System.out.println(i);
-            while(x>=xSize){
+            System.out.printf("****** SHIP %s ******\n",String.valueOf(i+1));
+            while(x>=xSize || x<0){
                 System.out.print("Gib x: ");
-                x = scan.nextInt();
+                x = scanner.nextInt();
             }
-            while(y>=ySize){
+            while(y>=ySize || y<0){
                 System.out.print("Gib y: ");
-                y = scan.nextInt();
+                y = scanner.nextInt();
             }
-            model[x][y]=1;
+            if(model[x][y]==1){
+                System.out.println("Cannot place a ship on another ship! Try again please!");
+                i-=1;
+            }else{
+                model[x][y]=1;
+            }
             x=xSize;
             y=ySize;
         }
-        scan.close();
+        printModel();
     }
 
-    // public void hit(int x, int y) {
-    //     if (model[x][y]==1) { //if ship present, hit
-    //         model[x][y]=2;
-    //     }
-    // }
-    // private void hitAction(){}
+
+
+    public void guessRoutine(){
+        int xi;
+        int yi;
+        while (this.ships != 0) {
+            System.out.print("Gib target x: ");
+            xi = scanner.nextInt();
+            System.out.print("Gib target y: ");
+            yi = scanner.nextInt();
+            guess(xi,yi);
+        }
+        printModel();
+    }
+
+    private void guess(int x, int y){
+        bombsDropped+=1;
+        if(model[x][y]==0){
+            model[x][y] = 2;
+            System.out.println("*** MISS!");
+        }else if (model[x][y]==1) {
+            model[x][y] = 3;
+            ships-=1;
+            if (this.ships==0){
+                System.out.printf("*** HIT! YOU HAVE WON! Bombs used: %s", bombsDropped);
+            }else{
+                System.out.printf("*** HIT! %s to go!\n",String.valueOf(this.ships));
+            }
+        }
+    }
 
 }
