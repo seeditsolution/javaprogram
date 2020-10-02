ccae8d53bbb4cf50fcb6c7e27b0130c3d5a82186
lpdubey9 <72254771+lpdubey9@users.noreply.github.com>
2020-10-02 16:02:44 +0530

    Create java program to reverse a string.java

---
+++ java program to reverse a string.java
@@ -0,0 +1,7 @@
+public class StringFormatter {  
+public static String reverseString(String str){  
+    StringBuilder sb=new StringBuilder(str);  
+    sb.reverse();  
+    return sb.toString();  
+}  
+}  
