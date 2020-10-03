6d7035fee02a85526a61193b10cbf2c70363332a
RAHUL CHOUDHARY <47866118+rahulchoudhary31@users.noreply.github.com>
2020-10-03 23:19:21 +0530

    greatest.java
    WAP to find out greatest no. in two no.

---
+++ greatest.java
@@ -0,0 +1,22 @@
+
+import java.util.*;
+class p4
+{
+public static void main(String [] args)
+{
+int a,b;
+Scanner sc=new Scanner(System.in);
+System.out.println( "Enter two no.:");
+a=sc.nextInt();
+b=sc.nextInt();
+if(a>b)
+{
+	System.out.println("Greatest no.="+a);
+}
+else
+{
+	System.out.println("Greatest no.="+b);
+	
+}
+}
+}
